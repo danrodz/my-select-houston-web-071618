083e56f3e8df5248a1564db6f51626902e31a77e
Daniel Rodriguez <danrodz15.dr@icloud.com>
2018-06-24 23:18:35 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,17 @@
-def my_select(collection)
- # your code here!
+def my_select(array)
+  new_array = []
+ if block_given?
+    i = 0 
+  
+    while i < array.length 
+      if yield array[i]
+        new_array.push(array[i])
+      end
+      i = i + 1 
+    end
+    
+    new_array
+  else
+    new_array
+  end
 end
